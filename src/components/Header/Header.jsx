32421cb73dc4947cb613aee3914ab8384491f9ce
Rinu2024 <rinu.work@gmail.com>
2024-05-22 19:37:21 -0400

    Adding Header and Logo Image

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+
+import "../../components/Header/Header.scss"
+import Image from "../../assets/logo/InStock-Logo.png"
+import { Link } from "react-router-dom";
+
+function Header(){
+    return(
+        <div className="header">
+            <div className="header__logo">
+                <img className="header__logoSize" src={Image} alt="Logo" />
+            </div>
+            <ul className="header__list">
+                <li  className="header__menu"><Link to="/warehouses">Warehouses</Link></li>
+                <li  className="header__menu"><Link to="/inventory">Inventory</Link></li>
+            </ul>
+       </div> 
+    );
+}
+
+export default Header;
