962d455008f5cb2e80111b6834a6e483b9c2318e
notaika <ercamanalo@gmail.com>
2024-05-21 19:47:04 -0700

    feat: installed react-router-dom and sass. added a start script in package.json. added all routes to possible pages + added placeholders for those pages. created a not found page. edited index.html and app.jsx to import scss instead of css.

---
+++ src/components/InventoryEdit/InventoryEdit.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function InventoryEdit() {
+  return (
+    <div>
+      <h1>Inventory - Edit</h1>
+      <p>Welcome to the inventory (edit) page!</p>
+    </div>
+  )
+}
---
+++ src/components/WarehouseDelete/WarehouseDelete.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WarehouseDelete() {
+  return (
+    <div>
+      <h1>Warehouse - Delete</h1>
+      <p>Welcome to the warehouse (delete) page!</p>
+    </div>
+  )
+}
