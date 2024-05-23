f32814b535d78011f91577fd80e23041d0b86049
notaika <ercamanalo@gmail.com>
2024-05-22 20:40:51 -0700

    feat: added and styled warehouse component

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import WarehouseList from '../../components/WarehouseList/WarehouseList';
 
 export default function WarehousePage() {
   return (
     <div>
       <h1>Warehouse</h1>
       <p>Welcome to the warehouse page!</p>
+      <WarehouseList />
     </div>
   )
 }
