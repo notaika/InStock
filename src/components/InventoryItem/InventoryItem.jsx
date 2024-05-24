62fc6d550e20158b23bf4d1f2a461c3e87d59718
Shirasa Prajapati <shirasapraja@gmail.com>
2024-05-23 21:54:11 -0600

    get inventory list using axios and display on page

--- src/components/InventoryItem/InventoryItem.jsx
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./InventoryItem.scss";
 
-export default function InventoryItem() {
+export default function InventoryItem({
+  name,
+  category,
+  status,
+  quantity,
+  warehouse,
+}) {
   return (
-    <div>
-      <h1>Inventory - Item</h1>
-      <p>Welcome to the inventory (item) page!</p>
+    <div className="table">
+      <div>
+        <h2>Inventory Item</h2>
+        <p>{name}</p>
+      </div>
+      <div>
+        <h2>Category</h2>
+        <p>{category}</p>
+      </div>
+      <div>
+        <h2>Status</h2>
+        <p>{status}</p>
+      </div>
+      <div>
+        <h2>Qty</h2>
+        <p>{quantity}</p>
+      </div>
+      <div>
+        <h2>Warehouse</h2>
+        <p>{warehouse}</p>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import InventoryItem from "../../components/InventoryItem/InventoryItem";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./InventoryPage.scss"
 
 export default function InventoryPage() {
+  const [inventory, setInventory] = useState([]);
+
+  useEffect(() => {
+    getInventories();
+  }, []);
+  async function getInventories() {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_LOCALHOST}/api/inventories`
+      );
+      setInventory(response.data);
+    } catch (error) {
+      console.error(`ERROR: Could not fetch inventory data`, error);
+    }
+  }
   return (
-    <div>
-      <h1>Inventory</h1>
-      <p>Welcome to the inventory page!</p>
-    </div>
-  )
+    <main className="inventories">
+      <div className="inventory-header">
+        <h1>Inventory</h1>
+        <input type="search" name="search" placeholder="Search..."/>
+        <button>Add New Item</button>
+      </div>
+      <div className="inventory-table">
+        <div className="table-heading">
+          <p>Inventory Item</p>
+          <p>Category</p>
+          <p>Status</p>
+          <p>Qty</p>
+          <p>Warehouse</p>
+        </div>
+        {inventory.map((item) => (
+          <InventoryItem
+            key={item.id}
+            name={item.item_name}
+            category={item.category}
+            status={item.status}
+            quantity={item.quantity}
+            warehouse={item.warehouse_name}
+          />
+        ))}
+      </div>
+      <div></div>
+    </main>
+  );
 }
