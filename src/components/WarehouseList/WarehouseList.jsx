c33c05f616469f9ae649f8850fc3b8d0560a11f0
notaika <ercamanalo@gmail.com>
2024-05-25 12:24:21 -0700

    feat: added warehouse search bar, add new warehouse button. incorporated header onto page. added sort img (functionality later) spacing errors and bug fixes.

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import chevronRight from "../../assets/icons/chevron_right-24px.svg";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import "./WarehouseList.scss";
 
-export default function WarehouseList ( { warehouse } ) {
+export default function WarehouseList({ warehouse }) {
   return (
     <>
       <section className="warehouse__list warehouse__list--mobile">
@@ -13,7 +12,7 @@ export default function WarehouseList ( { warehouse } ) {
           <div className="warehouse__list-container">
             <div className="warehouse__list--left">
               <h4 className="warehouse__list-title">Warehouse</h4>
-              <Link to="/:id" className="warehouse__list-nav">
+              <Link to={`/warehouse/${warehouse.id}`} className="warehouse__list-nav">
                 {warehouse.warehouse_name}
                 <img
                   src={chevronRight}
@@ -22,7 +21,9 @@ export default function WarehouseList ( { warehouse } ) {
                 />
               </Link>
               <h4 className="warehouse__list-title">Address</h4>
-              <p className="warehouse__address">{warehouse.address}, <br/> {warehouse.city}, {warehouse.country}</p>
+              <p className="warehouse__address">
+                {warehouse.address}, <br /> {warehouse.city}, {warehouse.country}
+              </p>
             </div>
             <div className="warehouse__list--right">
               <h4 className="warehouse__list-title">Contact Name</h4>
@@ -33,19 +34,20 @@ export default function WarehouseList ( { warehouse } ) {
             </div>
           </div>
           <div className="warehouse__list-functions">
-          <Link to={`warehouse/delete/${warehouse.id}`}className="warehouse__function-link"><img
+            <Link to={`warehouse/delete/${warehouse.id}`} className="warehouse__function-link">
+              <img
                 src={deleteIcon}
                 alt="Red garbage icon for delete button"
                 className="warehouse__list-delete"
               />
-          </Link>
-          <Link to={`/warehouse/edit/${warehouse.id}`} className="warehouse__function-link">
+            </Link>
+            <Link to={`/warehouse/edit/${warehouse.id}`} className="warehouse__function-link">
               <img
                 src={editIcon}
                 alt="A blue pen icon for edit button"
                 className="warehouse__list-edit"
               />
-              </Link>
+            </Link>
           </div>
         </div>
       </section>
@@ -61,25 +63,28 @@ export default function WarehouseList ( { warehouse } ) {
                 className="warehouse__list-title-logo"
               />
             </Link>
-            <p className="warehouse__address warehouse__info">{warehouse.address}, {warehouse.city}, {warehouse.country} </p>
+            <p className="warehouse__address warehouse__info">
+              {warehouse.address}, {warehouse.city}, {warehouse.country}
+            </p>
             <p className="warehouse__contact-name warehouse__info">{warehouse.contact_name}</p>
             <div className="warehouse__contact-container warehouse__info">
               <p className="warehouse__contact-information">{warehouse.contact_phone}</p>
               <p className="warehouse__contact-information">{warehouse.contact_email}</p>
             </div>
             <div className="warehouse__list-functions warehouse__info">
-              <Link to={`/warehouse/delete/${warehouse.id}`}className="warehouse__function-link"><img
-                src={deleteIcon}
-                alt="Red garbage icon for delete button"
-                className="warehouse__list-delete"
-              />
+              <Link to={`/warehouse/delete/${warehouse.id}`} className="warehouse__function-link">
+                <img
+                  src={deleteIcon}
+                  alt="Red garbage icon for delete button"
+                  className="warehouse__list-delete"
+                />
               </Link>
               <Link to={`/warehouse/edit/${warehouse.id}`} className="warehouse__function-link">
-              <img
-                src={editIcon}
-                alt="A blue pen icon for edit button"
-                className="warehouse__list-edit"
-              />
+                <img
+                  src={editIcon}
+                  alt="A blue pen icon for edit button"
+                  className="warehouse__list-edit"
+                />
               </Link>
             </div>
           </div>
--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,6 +1,11 @@
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import sortIcon from "../../assets/icons/sort-24px.svg";
+import "./WarehousePage.scss";
 
 export default function WarehousePage() {
   const [warehouses, setWarehouses] = useState([]);
@@ -21,22 +26,75 @@ export default function WarehousePage() {
   }, []);
 
   return (
-    <main className="warehouse"> 
-      <h1>Warehouse</h1>
-      <p>Welcome to the warehouse page!</p>
-      <div className="warehouse__list-titles">
-        <h4 className="warehouse__list-title">Warehouse</h4>
-        <h4 className="warehouse__list-title">Address</h4>
-        <h4 className="warehouse__list-title">Contact Name</h4>
-        <h4 className="warehouse__list-title">Contact Information</h4>
-        <h4 className="warehouse__list-title">Actions</h4>
-      </div>
+    <main className="home-page">
+      <div className="warehouse__wrapper">
+        <Header />
+        <article className="warehouse">
+          <div className="warehouse__header">
+            <h1 className="warehouse__title">Warehouses</h1>
+            <form className="warehouse__form">
+              <input
+                type="text"
+                className="warehouse__search-input"
+                placeholder="Search..."
+              />
+              <Link to="/warehouse/add" className="warehouse__search-btn">
+                + Add New Warehouse
+              </Link>
+            </form>
+          </div>
+          <div className="warehouse__list-titles">
+            <h4 className="warehouse__list-title">
+              Warehouse{" "}
+              <img
+                src={sortIcon}
+                alt="A sort icon"
+                className="warehouse__sort"
+              />
+            </h4>
+            <h4 className="warehouse__list-title">
+              Address
+              <img
+                src={sortIcon}
+                alt="A sort icon"
+                className="warehouse__sort"
+              />
+            </h4>
+            <h4 className="warehouse__list-title">
+              Contact Name
+              <img
+                src={sortIcon}
+                alt="A sort icon"
+                className="warehouse__sort"
+              />
+            </h4>
+            <h4 className="warehouse__list-title">
+              Contact Information
+              <img
+                src={sortIcon}
+                alt="A sort icon"
+                className="warehouse__sort"
+              />
+            </h4>
+            <h4 className="warehouse__list-title">
+              Actions
+              <img
+                src={sortIcon}
+                alt="A sort icon"
+                className="warehouse__sort"
+              />
+            </h4>
+          </div>
 
-    <section className="warehouse__container">
-      {warehouses.map((warehouse) => (
-        <WarehouseList warehouse={warehouse} key={warehouse.id} />
-      ))}
-    </section>
+          <section className="warehouse__container">
+            <div className="warehouse__map">
+              {warehouses.map((warehouse) => (
+                <WarehouseList warehouse={warehouse} key={warehouse.id} />
+              ))}
+            </div>
+          </section>
+        </article>
+      </div>
     </main>
   );
 }
