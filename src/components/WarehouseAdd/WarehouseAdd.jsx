946d5c2f869cd5c5a99357e3a907972dfd08b68e
Sufia Ali <sufiaali@Sufias-MacBook-Air-2.local>
2024-05-26 09:26:00 -0400

    text-input form parts implemented with lables and fields

--- src/components/WarehouseAdd/WarehouseAdd.jsx
+++ src/components/WarehouseAdd/WarehouseAdd.jsx
@@ -9,13 +9,162 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import { Link, useNavigate } from "react-router-dom";
 
-export default function WarehouseAdd() {
+
   const API_URL = import.meta.env.VITE_LOCALHOST;
 
+export default function WarehouseAdd() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      const response = await axios.post(`${API_URL}/api/warehouses`, values);
+      console.log("Warehouse added successfully:", response.data);
+      navigate("/");
+    } catch (error) {
+      setErrorMessage(error.response.data);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  const handleCancel = () => {
+    navigate("/");
+  };
+
+
   return (
-    <div>
-      <h1>Warehouse - Add</h1>
-      <p>Welcome to the warehouse (add) page!</p>
-    </div>
-  )
+    <>
+    <Formik
+      initialValues={{
+        warehouse_name: "",
+        street_address: "",
+        city: "",
+        country: "",
+        contact_name: "",
+        position: "",
+        phone_number: "",
+        email: "",
+      }}
+      validationSchema={Yup.object({
+        warehouse_name: Yup.string().required("This field is required"),
+        street_address: Yup.string().required("This field is required"),
+        city: Yup.string().required("This field is required"),
+        country: Yup.string().required("This field is required"),
+        contact_name: Yup.string().required("This field is required"),
+        position: Yup.string().required("This field is required"),
+        phone_number: Yup.string().required("This field is required"),
+        email: Yup.string().required("This field is required").email("Invalid email address"),
+      })}
+    >
+    {({ values }) => (
+      <Form className="warehouse-add">
+        <div className="warehouse-add__header">
+          <div className="warehouse-add__header-box">
+            <Link to="/">
+            <img
+              src={arrowIcon}
+              alt="Back arrow"
+              className="warehouse-add__header-icon"
+            />
+            </ Link>
+            <h1 className="warehouse-add__title">Add New Warehouse</h1>
+          </div>
+        </div>
+
+
+        <div className="warehouse-add__fields-container">
+          <div className="warehouse-add__left-container">
+            <h2 className="warehouse-add__subtitle">Warehouse Details</h2>
+            <TextInput
+              label="Warehouse Name"
+              name="warehouse_name"
+              type="text"
+              placeholder="Warehouse Name"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+            <TextInput
+              label="Street Address"
+              name="street_address"
+              type="text"
+              placeholder="Street Address"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+            <TextInput
+              label="City"
+              name="city"
+              type="text"
+              placeholder="City"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+            <TextInput
+              label="Country"
+              name="country"
+              type="text"
+              placeholder="Country"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+          </div>
+
+
+          <div className="warehouse-add__right-container">
+            <h2 className="warehouse-add__subtitle">Contact Details</h2>
+            <TextInput
+              label="Contact Name"
+              name="contact_name"
+              type="text"
+              placeholder="Contact Name"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+            <TextInput
+              label="Position"
+              name="position"
+              type="text"
+              placeholder="Position"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+            <TextInput
+              label="Phone Number"
+              name="phone_number"
+              type="text"
+              placeholder="Phone Number"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+            <TextInput
+              label="Email"
+              name="email"
+              type="text"
+              placeholder="Email"
+              className="warehouse-add__input"
+              labelClassName="warehouse-add__label"
+            />
+          </div>
+        </div>
+
+
+        <div className="warehouse-add__button-container">
+          <button type="button" className="warehouse-add__button-item--left"
+           >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="warehouse-add__button-item--right"
+          >
+            + Add Warehouse
+          </button>
+        </div>
+      </Form>
+    )}
+  </Formik>
+</>
+);
 }
+
+
